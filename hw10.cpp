3a53bc1565547502132f1fb52cdd0e24e30d0c26
s1120337-ship-it <s1120337@dmhs.kh.edu.tw>
2025-11-27 14:21:04 +0800

    Add files via upload

---
+++ hw10.cpp
@@ -0,0 +1,53 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include <cstdlib>  // rand(), srand()
+#include <ctime>    // time()
+
+using namespace std;
+
+int main() {
+    const int N = 10;   // 陣列大小
+    int arr[N];
+
+    // ---- 隨機數產生 ----
+    srand(time(NULL));
+
+    cout << "原始亂數陣列: ";
+    for (int i = 0; i < N; i++) {
+        arr[i] = rand() % 100;    // 0~99
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    // ---- 選擇排序（Selection Sort）----
+    for (int i = 0; i < N - 1; i++) {
+        int minIndex = i;
+
+        for (int j = i + 1; j < N; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        // 交換
+        int temp = arr[i];
+        arr[i] = arr[minIndex];
+        arr[minIndex] = temp;
+    }
+
+    // ---- 排序後結果 ----
+    cout << "排序後陣列: ";
+    for (int i = 0; i < N; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
